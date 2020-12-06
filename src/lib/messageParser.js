cc9bd0343b869cc2226dad241c7cd962a90e7064
zacharyberson <zacharyberson@gmail.com>
2020-12-05 19:10:50 -0500

    feature: separated commands to regular, sound, and regex triggered feature: added support for hidden commands and sounds chore: added id to Image properties

--- src/lib/messageParser.js
+++ src/lib/messageParser.js
@@ -1,16 +1,16 @@
-import { commands, regex } from './commands.js';
+import { commands, hiddenCommands, regex, sounds, hiddenSounds } from './commands.js';
 
 const processMessage = (target, context, command) => {
     const commandParts = command.split(' ', 2);
     const commandName = commandParts[0];
     const commandQuery = commandParts.length > 1 ? commandParts[1] : '';
 
-    // try to match entire string to known command regex
-    Object.keys(regex).forEach((value) => {
-        if (command.match(new RegExp(value))) {
+    // try to match first word to known command
+    Object.keys(commands).forEach((value) => {
+        if (commandName === value) {
             try {
-                regex[value](target, context, command);
-                console.log(`* Matched ${value} regex`);
+                commands[value](target, context, commandQuery);
+                console.log(`* Executed ${value} command`);
                 return;
             } catch (e) {
                 console.error(e);
@@ -19,11 +19,50 @@ const processMessage = (target, context, command) => {
     });
 
     // try to match first word to known command
-    Object.keys(commands).forEach((value) => {
+    Object.keys(hiddenCommands).forEach((value) => {
         if (commandName === value) {
             try {
-                commands[value](target, context, commandQuery);
+                hiddenCommands[value](target, context, commandQuery);
                 console.log(`* Executed ${value} command`);
+                return;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+
+    // try to match first word to known sound
+    Object.keys(sounds).forEach((value) => {
+        if (commandName === value) {
+            try {
+                sounds[value](target, context, commandQuery);
+                console.log(`* Played ${value} sound`);
+                return;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+
+    // try to match first word to known sound
+    Object.keys(hiddenSounds).forEach((value) => {
+        if (commandName === value) {
+            try {
+                hiddenSounds[value](target, context, commandQuery);
+                console.log(`* Played ${value} sound`);
+                return;
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
+
+    // try to match entire string to known command regex
+    Object.keys(regex).forEach((value) => {
+        if (command.match(new RegExp(value))) {
+            try {
+                regex[value](target, context, command);
+                console.log(`* Matched ${value} regex`);
                 return;
             } catch (e) {
                 console.error(e);
